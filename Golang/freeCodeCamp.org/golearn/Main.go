f4c0830e6ea22343e2f9be623be0f6b1c4a81bfd
Rohan Mathew <robozmat@gmail.com>
2020-12-21 11:40:36 +0000

    update

--- Golang/freeCodeCamp.org/golearn/Main.go
+++ Golang/freeCodeCamp.org/golearn/Main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	_, _ = fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			_, _ = fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+	fmt.Println(req.Form)
+}
+
 func main() {
 
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	_ = http.ListenAndServe(":8090", nil)
 }
--- Golang/freeCodeCamp.org/golearn/boseDim.go
+++ Golang/freeCodeCamp.org/golearn/boseDim.go
@@ -4,15 +4,32 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	response, _ := http.Get("http://192.168.0.11:8090/volume")
+	ip := os.Args[1]
+	dimInterval, _ := time.ParseDuration(os.Args[2])
+
+	response, _ := http.Get(fmt.Sprintf("http://%v:8090/volume", ip))
 	body, _ := ioutil.ReadAll(response.Body)
-	stringBod := string(body)
-	
+	responseBody := string(body)
+
+	volIdxStart := strings.Index(responseBody, "<targetvolume>") + 14
+	volIdxEnd := strings.Index(responseBody, "</targetvolume>")
+	volume, _ := strconv.Atoi(responseBody[volIdxStart:volIdxEnd])
+
+	for i := volume; i > 0; i-- {
+		time.Sleep(dimInterval)
+		_, _ = http.Post(fmt.Sprintf("http://%v:8090/volume", ip),
+			"text/xml",
+			strings.NewReader(getPostXML(i)))
+	}
 }
 
-func getPostXML(volume uint8) string {
+func getPostXML(volume int) string {
 	return fmt.Sprintf("<volume>%v</volume>", volume)
 }
